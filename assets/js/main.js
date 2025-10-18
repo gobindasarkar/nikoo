60feddaff3cb055d6e303c9460fd197efa6d1981
gobinda sarkar <55000159+gobindasarkar@users.noreply.github.com>
2025-10-18 09:25:56 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+(function ($) {
+  "use strict";
+  /*-------------------------------------------
+  preloader active
+  --------------------------------------------- */
+  jQuery(window).on("load", function () {
+    jQuery(".preloader").fadeOut("slow");
+  });
+
+  $(window).on("scroll", function () {
+    const scrolled = $(this).scrollTop() >= 300;
+    $(".header-aera").toggleClass("stick", scrolled);
+  });
+
+  jQuery(document).ready(function () {
+    /*-------------------------------------------
+    js scrollup
+    --------------------------------------------- */
+    $.scrollUp({
+      scrollText: '<i class="fa fa-angle-up"></i>',
+      easingType: "linear",
+      scrollSpeed: 900,
+      animation: "fade",
+    });
+    /*-------------------------------------------
+      hero-banner-slider active
+    --------------------------------------------- */
+    $(".hero-banner-slider").slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: false,
+      dots: false,
+      arrows: true,
+      prevArrow: '<i class="slick-prev fa-solid fa-angle-left"></i>',
+      nextArrow: '<i class="slick-next fa-solid fa-angle-right"></i>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+   
+    
+  });
+})(jQuery);
